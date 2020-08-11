6b5df5015a79194d661b50a31d1a90e239d8b77d
jareerzeenam <jareerzeenam.28@gmail.com>
2020-08-11 22:00:34 +0530

    Initial Project Setup Done

--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// !imports
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+
+import "popper.js";
+import "bootstrap";
+import "./assets/app.scss";
+
+// ! Import Navbar Globally
+Vue.component("Navbar", require("./components/Navbar.vue").default);
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
